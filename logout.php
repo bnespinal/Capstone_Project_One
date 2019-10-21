c2f2e4ec2e55a00c9f9ca44e248dc8be49d99301
bnespinal <54385953+bnespinal@users.noreply.github.com>
2019-10-21 08:48:39 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: jennis
- * Date: 11/29/2017
- * Time: 10:40 PM
+ * User: bnespinal
+ * Date: 9/6/2019
+ * Time: 4:15 PM
  */
 session_start();
 session_unset();
